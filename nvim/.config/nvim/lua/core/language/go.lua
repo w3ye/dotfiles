f3a8f1cd59a8604ba2ae003b6f6421a7ce380884
William Ye <33244107+w3ye@users.noreply.github.com>
2024-08-03 23:04:25 -0400

    move go to its own file

---
+++ nvim/.config/nvim/lua/core/language/go.lua
@@ -0,0 +1,29 @@
+local M = {
+	"ray-x/go.nvim",
+	enabled = _G.enabled.go,
+	dependencies = { -- optional packages
+		"ray-x/guihua.lua",
+		"neovim/nvim-lspconfig",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("go").setup()
+		local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.go",
+			callback = function()
+				require("go.format").goimport()
+			end,
+			group = format_sync_grp,
+		})
+	end,
+	keys = {
+		{ "<leader>tf", "<cmd>GoTestFile<cr>", desc = "Go Test File" },
+		{ "<leader>tF", "<cmd>GoTestFunc<cr>", desc = "Go Test Func" },
+	},
+	event = { "CmdlineEnter" },
+	ft = { "go", "gomod" },
+	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+}
+
+return M
--- nvim/.config/nvim/lua/core/language/init.lua
+++ nvim/.config/nvim/lua/core/language/init.lua
@@ -1,46 +1,7 @@
+local go = require("core.language.go")
+
 local M = {
-	{
-		"ray-x/go.nvim",
-		enabled = _G.enabled.go,
-		dependencies = { -- optional packages
-			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = function()
-			require("go").setup()
-			local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*.go",
-				callback = function()
-					require("go.format").goimport()
-				end,
-				group = format_sync_grp,
-			})
-		end,
-		keys = {
-			{ "<leader>tf", "<cmd>GoTestFile<cr>", desc = "Go Test File" },
-			{ "<leader>tF", "<cmd>GoTestFunc<cr>", desc = "Go Test Func" },
-		},
-		event = { "CmdlineEnter" },
-		ft = { "go", "gomod" },
-		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-	},
-	-- {
-	-- 	"nvim-neotest/neotest",
-	-- 	enabled = _G.enabled.go,
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"nvim-treesitter/nvim-treesitter",
-	-- 		"antoinemadec/FixCursorHold.nvim",
-	-- 		"thenbe/neotest-playwright",
-	-- 	},
-	-- 	keys = require("core.neotest").keys,
-	-- 	config = function()
-	-- 		require("core.neotest").neotest_setup()
-	-- 	end,
-	-- 	event = "BufEnter *.spec.ts",
-	-- },
+	go,
 }
 
 return M
