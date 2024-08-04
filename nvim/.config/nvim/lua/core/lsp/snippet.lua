644efbf0947b76e7da11dc7a76de164b2a49a908
William Ye <33244107+w3ye@users.noreply.github.com>
2024-08-03 23:06:04 -0400

    updating enabled of snippet

--- nvim/.config/nvim/lua/core/lsp/snippet.lua
+++ nvim/.config/nvim/lua/core/lsp/snippet.lua
@@ -1,12 +1,11 @@
 local enabled = _G.enabled.cmp
 local M = {
 	{ "rafamadriz/friendly-snippets", enabled = enabled },
-	"saadparwaiz1/cmp_luasnip",
+	{ "saadparwaiz1/cmp_luasnip", enabled = enabled },
 	{
 		"L3MON4D3/LuaSnip",
+		enabled = enabled,
 		-- follow latest release.
-		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-		-- install jsregexp (optional!).
 		build = "make install_jsregexp",
 		config = function()
 			require("luasnip.loaders.from_vscode").lazy_load()
