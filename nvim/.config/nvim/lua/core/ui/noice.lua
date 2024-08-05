aad532bce2bd713de8536248ab345bfc557df1b7
William Ye <33244107+w3ye@users.noreply.github.com>
2024-08-04 20:27:20 -0400

    update noice

--- nvim/.config/nvim/lua/core/ui/noice.lua
+++ nvim/.config/nvim/lua/core/ui/noice.lua
@@ -9,47 +9,9 @@ local routes = {
 	},
 	{
 		filter = {
-			event = "msg_show.echo",
-			kind = "",
-			find = "change",
-		},
-		opts = { skip = true },
-	},
-	{
-		filter = {
 			event = "msg_show",
 			kind = "",
-			find = "[supermaven-nvim]",
-		},
-		opts = { skip = true },
-	},
-	{
-		filter = {
-			event = "msg_show",
-			kind = "",
-			find = "lines",
-		},
-		opts = { skip = true },
-	},
-	{
-		filter = {
-			event = "msg_show",
-			kind = "",
-			find = "seconds ago",
-		},
-		opts = { skip = true },
-	},
-	{
-		filter = {
-			event = "msg_show",
-			min_height = 10,
-			["not"] = { kind = { "search_count", "echo" } },
-		},
-	},
-	{
-		filter = {
-			event = "notify.info",
-			kind = "",
+			find = "supermaven",
 		},
 		opts = { skip = true },
 	},
@@ -57,49 +19,28 @@ local routes = {
 
 local M = {
 	"folke/noice.nvim",
+	event = "VeryLazy",
 	enabled = _G.enabled.noice,
-	lazy = false,
-	config = function(_, opts)
-		require("noice").setup(opts)
-	end,
 	opts = {
-		views = {
-			cmdline_popup = {
-				border = {
-					style = "none",
-					padding = { 2, 3 },
-				},
-				filter_options = {},
-				win_options = {
-					winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-				},
-			},
-		},
-		lsp = {
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
-			},
-		},
-		-- you can enable a preset for easier configuration
-		presets = {
-			bottom_search = true, -- use a classic bottom cmdline for search
-			command_palette = true, -- position the cmdline and popupmenu together
-			long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = false, -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = false, -- add a border to hover docs and signature help
+		redirect = {
+			view = "messages",
+			filter = { event = "msg_show" },
 		},
 		routes = routes,
 		messages = {
+			-- NOTE: If you enable messages, then the cmdline is enabled automatically.
+			-- This is a current Neovim limitation.
 			enabled = true, -- enables the Noice messages UI
-			view = false, -- default view for messages
-			view_error = false, -- view for errors
-			view_warn = false, -- view for warnings
-			view_history = false, -- view for :messages
+			view = "notify", -- default view for messages
+			view_error = "split", -- view for errors
+			view_warn = "split", -- view for warnings
+			view_history = "messages", -- view for :messages
 			view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
 		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
 	},
 	keys = {
 		{
