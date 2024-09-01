39f9e8d801ef5084d2a993d6c01e1a13a20eafd7
William Ye <33244107+w3ye@users.noreply.github.com>
2024-08-31 22:10:43 -0400

    [nvim] config formatter

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -5,16 +5,22 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			typescriptreact = { "prettier" },
+			javascript = { "prettierd" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
 			go = { "gofmt" },
 			rust = { "rustfmt" },
 		},
-		format_on_save = function(bufnr)
-			vim.lsp.buf.format({ bufnr = bufnr })
-			return { timeout_ms = 5000, lsp_fallback = true }
-		end,
+		-- format_on_save = function(bufnr)
+		-- 	vim.lsp.buf.format({ bufnr = bufnr, quiet = true, async = true })
+		-- 	return { timeout_ms = 5000, lsp_fallback = true }
+		-- end,
+		format_on_save = {
+			lsp_format = "fallback",
+			timeout_ms = 500,
+			quiet = true,
+		},
+		quiet = true,
 		format_after_save = {
 			lsp_format = true,
 		},
