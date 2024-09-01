2f88a4695a689da637c9582c883aa54c9d6adb59
William Ye <33244107+w3ye@users.noreply.github.com>
2024-08-31 23:14:05 -0400

    [nvim] update ts ft

--- nvim/.config/nvim/lua/plugins/tstools.lua
+++ nvim/.config/nvim/lua/plugins/tstools.lua
@@ -13,6 +13,7 @@ return {
 	"pmizio/typescript-tools.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 	event = "VeryLazy",
+	ft = { "typescript", "javascript" },
 	setup = function()
 		require("typescript-tools").setup(options)
 	end,
