50484bf8647fb0f95e565ae002c29a7e60c0a343
William Ye <wy@Williams-MacBook-Pro.local>
2024-09-03 01:12:03 -0400

    [nvim] move bookmarks to it's own file

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -65,14 +65,13 @@ local telescope_setup = function()
 	require("telescope").load_extension("fzf")
 	require("telescope").load_extension("live_grep_args")
 	-- require("telescope").load_extension("projects")
-	require("telescope").load_extension("bookmarks")
 end
 
 local telescope_keys = {
 	{ "<C-f>", "<cmd>Telescope find_files<cr>" },
 	{ "<leader>fA", "<cmd>Telescope live_grep<cr>" },
 	{ "<leader>fa", "<cmd>Telescope live_grep_args<cr>" },
-	{ "<leader>fs", "<cmd>Telescope grep_string<cr>" },
+	{ "<leader>fs", "<cmd>Telescope grep_string<cr>", mode = { "n", "v" } },
 	{ "<leader>fo", "<cmd>Telescope oldfiles<cr>" },
 	{ "<leader>f", "<cmd>Telescope<cr>" },
 	{ "<leader>ff", "<cmd>Telescope buffers<cr>" },
@@ -97,52 +96,4 @@ return {
 		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 	},
 	{ "nvim-telescope/telescope-live-grep-args.nvim" },
-	{
-		"tomasky/bookmarks.nvim",
-		event = "VimEnter",
-		opts = {
-			sign_priority = 8, --set bookmark sign priority to cover other sign
-			save_file = vim.fn.expand("$HOME/.bookmarks"), -- bookmarks save file path
-			keywords = {
-				["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
-				["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
-				["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
-				["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
-			},
-			on_attach = function(bufnr)
-				local bm = require("bookmarks")
-				local map = vim.keymap.set
-				map("n", "mm", bm.bookmark_toggle) -- add or remove bookmark at current line
-				map("n", "mi", bm.bookmark_ann) -- add or edit mark annotation at current line
-				map("n", "mc", bm.bookmark_clean) -- clean all marks in local buffer
-				map("n", "mn", bm.bookmark_next) -- jump to next mark in local buffer
-				map("n", "mp", bm.bookmark_prev) -- jump to previous mark in local buffer
-				map("n", "ml", bm.bookmark_list) -- show marked file list in quickfix window
-				map("n", "mx", bm.bookmark_clear_all) -- show marked file list in quickfix window
-			end,
-		},
-		-- config = function()
-		-- 	require("bookmarks").setup({
-		-- 		sign_priority = 8, --set bookmark sign priority to cover other sign
-		-- 		save_file = vim.fn.expand("$HOME/.bookmarks"), -- bookmarks save file path
-		-- 		keywords = {
-		-- 			["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
-		-- 			["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
-		-- 			["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
-		-- 			["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
-		-- 		},
-		-- 		on_attach = function(bufnr)
-		-- 			local bm = require("bookmarks")
-		-- 			local map = vim.keymap.set
-		-- 			map("n", "mm", bm.bookmark_toggle) -- add or remove bookmark at current line
-		-- 			map("n", "mi", bm.bookmark_ann) -- add or edit mark annotation at current line
-		-- 			map("n", "mc", bm.bookmark_clean) -- clean all marks in local buffer
-		-- 			map("n", "mn", bm.bookmark_next) -- jump to next mark in local buffer
-		-- 			map("n", "mp", bm.bookmark_prev) -- jump to previous mark in local buffer
-		-- 			map("n", "ml", bm.bookmark_list) -- show marked file list in quickfix window
-		-- 			map("n", "mx", bm.bookmark_clear_all) -- show marked file list in quickfix window
-		-- 		end,
-		-- 	})
-		-- end,
-	},
 }
