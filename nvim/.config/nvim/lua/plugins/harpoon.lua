2d94f6bae3ffdeb727977382a49c448ff434861e
William Ye <wy@Williams-MacBook-Pro.local>
2024-09-03 01:12:13 -0400

    [nvim] update harpoon keybindings

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -6,6 +6,6 @@ return {
 		{ "<leader>hm", '<cmd>lua require("harpoon.mark").add_file()<cr>', desc = "harpoon mark" },
 		{ "<leader>ht", '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', desc = "harpoon toggle" },
 		{ "<leader>hn", '<cmd>lua require("harpoon.ui").nav_next()<cr>', desc = "harpoon next" },
-		{ "<leader>hp", '<cmd>lua require("harpoon.ui").nav_prev()<cr>', desc = "harpoon previous" },
+		{ "<leader>hh", '<cmd>lua require("harpoon.ui").nav_prev()<cr>', desc = "harpoon previous" },
 	},
 }
