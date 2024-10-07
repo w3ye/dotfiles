2494ba8eca99af404cbe6894081253284cbf1307
William Ye <william.y@roserocket.com>
2024-10-07 19:23:48 -0400

    [nvim] update mono repo

--- nvim/.config/nvim/lua/plugins/monorepo.lua
+++ nvim/.config/nvim/lua/plugins/monorepo.lua
@@ -1,7 +1,11 @@
 return {
 	"imNel/monorepo.nvim",
 	config = function()
-		require("monorepo").setup({})
+		require("monorepo").setup({
+			silent = false, -- Supresses vim.notify messages
+			autoload_telescope = true, -- Automatically loads the telescope extension at setup
+			data_path = vim.fn.stdpath("data"), -- Path that monorepo.json gets saved to
+		})
 	end,
 	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 	keys = {
@@ -10,14 +14,28 @@ return {
 			function()
 				require("telescope").extensions.monorepo.monorepo()
 			end,
-			desc = "Toggle list",
+			desc = "monorepo",
 		},
 		{
 			"<leader>ma",
 			function()
+				require("monorepo").add_project()
+			end,
+			desc = "monorepo add project",
+		},
+		{
+			"<leader>mr",
+			function()
+				require("monorepo").remove_project()
+			end,
+			desc = "monorepo remove project",
+		},
+		{
+			"<leader>n",
+			function()
 				require("monorepo").toggle_project()
 			end,
-			desc = "Toggle Monorepo",
+			desc = "monorepo toggle project",
 		},
 	},
 }
