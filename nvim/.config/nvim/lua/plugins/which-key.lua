94f7edcb9fb0931c23f6eebd74fd73259a2f98b2
William Ye <wy@Williams-MacBook-Pro.local>
2025-01-12 02:17:30 +0200

    [nvim] update which-key

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,9 +1,20 @@
 return {
 	"folke/which-key.nvim",
-	lazy = true,
 	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 500
-	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		preset = "modern",
+		win = {},
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
