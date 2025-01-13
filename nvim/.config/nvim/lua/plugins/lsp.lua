f610f4743148d41cbe33b91f229c7f9c7aefd0dc
William Ye <wy@Williams-MacBook-Pro.local>
2025-01-13 21:36:22 +0200

    [lsp] update lsp config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,46 +1,5 @@
 return {
 	{
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		dependencies = "mason.nvim",
-		cmd = { "DapInstall", "DapUninstall" },
-		opts = {
-			-- Makes a best effort to setup the various debuggers with
-			-- reasonable debug configurations
-			automatic_installation = false,
-
-			-- You can provide additional configuration to the handlers,
-			-- see mason-nvim-dap README for more information
-			handlers = {},
-
-			-- You'll need to check that you have the required things installed
-			-- online, please don't ask me how to install them :)
-			ensure_installed = {
-				-- Update this to ensure that you have the debuggers for the langs you want
-			},
-		},
-		-- mason-nvim-dap is loaded when nvim-dap loads
-		config = function() end,
-		init = function()
-			local severity = vim.diagnostic.severity
-			vim.diagnostic.config({
-				virtual_text = false,
-				underline = false,
-				signs = {
-					text = {
-						[severity.ERROR] = "",
-						[severity.WARN] = " ",
-						[severity.HINT] = " ",
-						[severity.INFO] = " ",
-					},
-				},
-			})
-		end,
-	},
-	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			"SmiteshP/nvim-navbuddy",
@@ -52,44 +11,6 @@ return {
 			keys = { { "<leader>o", mode = { "n" }, "<cmd>Navbuddy<cr>", desc = "toggle Navbuddy" } },
 		},
 		config = function()
-			require("mason").setup({
-				ensure_installed = {
-					"tsserver",
-					"eslint",
-					"lua_ls",
-				},
-			})
-			require("mason-lspconfig").setup()
-
-			require("mason-lspconfig").setup_handlers({
-				function(server_name) -- default handler (optional)
-					-- getting the filename
-					require("lspconfig")[server_name].setup({
-						settings = {
-							maxPreload = 10000,
-							preloadFileSize = 1000,
-							allow_incremental_sync = true,
-						},
-					})
-				end,
-				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup({
-						settings = {
-							Lua = {
-								diagnostics = {
-									globals = { "vim" },
-								},
-							},
-						},
-					})
-				end,
-				["ts_ls"] = function()
-					require("plugins.tstools").setup()
-				end,
-				["glint"] = function() end,
-			})
-
 			-- Global mappings.
 			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
@@ -109,36 +30,31 @@ return {
 					-- Buffer local mappings.
 					-- See `:help vim.lsp.*` for documentation on any of the below functions
 					local opts = { buffer = ev.buf }
-					-- vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
 					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 					-- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 					vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-					vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-					vim.keymap.set("n", "<space>wl", function()
+					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set("n", "<leader>wl", function()
 						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 					end, opts)
-					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-					-- vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set("n", "<space>rs", vim.diagnostic.reset, opts)
-					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set("n", "<leader>rs", vim.diagnostic.reset, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 					vim.keymap.set("n", "gv", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", opts)
-					vim.keymap.set("n", "<space>F", function()
+					vim.keymap.set("n", "<leader>F", function()
 						vim.lsp.buf.format({ async = true })
 					end, opts)
 				end,
 			})
 		end,
+		---@diagnostic disable-next-line: unused-local
 		on_attach = function(client, bufnr)
 			client.server_capabilities.semanticTokensProvider = nil
 		end,
-		-- opts = {
-		-- 	diagnostics = {
-		-- 		virtual_text = false,
-		-- 	},
-		-- },
 	},
 }
