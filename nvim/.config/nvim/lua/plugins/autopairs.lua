18147167e9cd0b5bf60e2a69c02195ffe5749896
William Ye <33244107+w3ye@users.noreply.github.com>
2025-02-12 19:52:20 -0500

    [nvim] disable autopairs

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,16 +1,17 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "VeryLazy",
-	opts = {
-		fast_wrap = {
-			map = "<M-e>",
-			chars = { "{", "[", "(", '"', "'" },
-			pattern = [=[[%'%"%)%>%]%)%}%,]]=],
-			end_key = "$",
-			keys = "qwertyuiopzxcvbnmasdfghjkl",
-			check_comma = true,
-			highlight = "Search",
-			highlight_grey = "Comment",
-		},
-	},
+  "windwp/nvim-autopairs",
+  enabled = false,
+  event = "VeryLazy",
+  opts = {
+    fast_wrap = {
+      map = "<M-e>",
+      chars = { "{", "[", "(", '"', "'" },
+      pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+      end_key = "$",
+      keys = "qwertyuiopzxcvbnmasdfghjkl",
+      check_comma = true,
+      highlight = "Search",
+      highlight_grey = "Comment",
+    },
+  },
 }
