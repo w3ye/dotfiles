f733d9d292d6dd999ba799615ad71cfab6cde387
William Ye <33244107+w3ye@users.noreply.github.com>
2025-02-12 19:51:59 -0500

    [nvim] add mini to replace some plugins

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,10 @@
+return {
+  'echasnovski/mini.nvim',
+  version = false,
+  config = function()
+    require('mini.ai').setup()
+    require('mini.surround').setup()
+    require('mini.pairs').setup()
+  end,
+  keys = {}
+}
