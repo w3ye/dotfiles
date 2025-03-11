ada54cf20fdc3625207cc03f24420660bc46ec6f
William Ye <33244107+w3ye@users.noreply.github.com>
2025-03-11 01:52:49 -0400

    [nvim] update bindings on snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,9 +1,41 @@
 return {
   "folke/snacks.nvim",
   opts = function(_, opts)
-    opts.picker.win.input.keys = {
-      ["<a-.>"] = { "toggle_hidden", mode = { "i", "n" } },
-    }
+    opts.picker.win.input.keys["<a-.>"] = { "toggle_hidden", mode = { "i", "n" } }
     return opts
   end,
+  keys = {
+    {
+      "<leader><space>",
+      LazyVim.pick("smart", {
+        multi = { "buffers", "recent", "files" },
+        format = "file", -- use `file` format for all sources
+        matcher = {
+          cwd_bonus = true, -- boost cwd matches
+          frecency = true, -- use frecency boosting
+          sort_empty = true, -- sort even when the filter is empty
+        },
+        transform = "unique_file",
+        layout = { preset = "vscode" },
+      }),
+      desc = "Smart Find Files",
+    },
+    {
+      "<leader>ff",
+      LazyVim.pick("files", { layout = { preset = "vscode" }, hidden = true }),
+      desc = "Find Files",
+    },
+    {
+      "<leader>sw",
+      LazyVim.pick("grep_word", { regex = true }),
+      desc = "Visual selection or word",
+      mode = { "n", "x" },
+    },
+    {
+      "<leader>sW",
+      LazyVim.pick("grep_word", { root = false }),
+      desc = "Visual selection or word (cwd)",
+      mode = { "n", "x" },
+    },
+  },
 }
